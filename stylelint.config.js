407d85a835ecce03369f5b6cd8e4d2592b211b9a
nihaohaha <2797884436@qq.com>
2023-03-07 13:36:03 +0800

    chore: 添加stylelint配置

---
+++ stylelint.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  plugins: ['stylelint-order'],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue', '*.html', '**/*.html'],
+      extends: ['stylelint-config-recommended'],
+      rules: {
+        'keyframes-name-pattern': null,
+        'selector-pseudo-class-no-unknown': [
+          true,
+          {
+            ignorePseudoClasses: ['deep', 'global']
+          }
+        ],
+        'selector-pseudo-element-no-unknown': [
+          true,
+          {
+            ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted']
+          }
+        ]
+      }
+    },
+    {
+      files: ['*.less', '**/*.less'],
+      extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue']
+    }
+  ]
+}
