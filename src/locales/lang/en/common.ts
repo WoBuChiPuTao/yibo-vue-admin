ac326a9bd64c624a70aef43da8a2fd10c55e744d
nihaohaha <2797884436@qq.com>
2023-03-09 15:18:38 +0800

    wip: setting模块的语言配置

--- src/locales/lang/en/common.ts
+++ src/locales/lang/en/common.ts
@@ -16,5 +16,41 @@ export default {
   back: 'Back',
 
   light: 'Light',
-  dark: 'Dark'
+  dark: 'Dark',
+  header: {
+    // user dropdown
+    dropdownItemDoc: 'Document',
+    dropdownItemLoginOut: 'Log Out',
+
+    tooltipErrorLog: 'Error log',
+    tooltipLock: 'Lock screen',
+    tooltipNotify: 'Notification',
+
+    tooltipEntryFull: 'Full Screen',
+    tooltipExitFull: 'Exit Full Screen',
+
+    // lock
+    lockScreenPassword: 'Lock screen password',
+    lockScreen: 'Lock screen',
+    lockScreenBtn: 'Locking',
+
+    home: 'Home'
+  },
+  multipleTab: {
+    reload: 'Refresh current',
+    close: 'Close current',
+    closeLeft: 'Close Left',
+    closeRight: 'Close Right',
+    closeOther: 'Close Other',
+    closeAll: 'Close All'
+  },
+  setting: {
+    drawerTitle: 'Configuration',
+
+    darkMode: 'Dark mode',
+
+    on: 'On',
+    off: 'Off',
+    minute: 'Minute'
+  }
 }
--- src/locales/lang/zh_cn/common.ts
+++ src/locales/lang/zh_cn/common.ts
@@ -16,5 +16,42 @@ export default {
   back: '返回',
 
   light: '亮色主题',
-  dark: '黑暗主题'
+  dark: '黑暗主题',
+  header: {
+    // user dropdown
+    dropdownItemDoc: '文档',
+    dropdownItemLoginOut: '退出系统',
+
+    // tooltip
+    tooltipErrorLog: '错误日志',
+    tooltipLock: '锁定屏幕',
+    tooltipNotify: '消息通知',
+
+    tooltipEntryFull: '全屏',
+    tooltipExitFull: '退出全屏',
+
+    // lock
+    lockScreenPassword: '锁屏密码',
+    lockScreen: '锁定屏幕',
+    lockScreenBtn: '锁定',
+
+    home: '首页'
+  },
+  multipleTab: {
+    reload: '重新加载',
+    close: '关闭标签页',
+    closeLeft: '关闭左侧标签页',
+    closeRight: '关闭右侧标签页',
+    closeOther: '关闭其它标签页',
+    closeAll: '关闭全部标签页'
+  },
+  setting: {
+    drawerTitle: '项目配置',
+
+    darkMode: '主题',
+
+    on: '开',
+    off: '关',
+    minute: '分钟'
+  }
 }
