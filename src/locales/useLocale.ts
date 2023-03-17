a93e45e2f64d1d81345b4222c2cc89dbf940a8d3
nihaohaha <2797884436@qq.com>
2023-03-17 17:43:00 +0800

    fix: eslint格式化问题

--- src/locales/useLocale.ts
+++ src/locales/useLocale.ts
@@ -2,6 +2,7 @@ import { LocaleType } from '#/store'
 import { useLocaleStore } from '@/store/modules/locale'
 import { computed, unref } from 'vue'
 import { setHtmlPageLang, loadLocale } from './helper'
+
 import { i18n } from './setupI18n'
 
 function setI18nLanguage(locale: LocaleType) {
@@ -10,7 +11,8 @@ function setI18nLanguage(locale: LocaleType) {
   if (i18n.mode === 'legacy') {
     i18n.global.locale = locale
   } else {
-    ;(i18n.global.locale as any).value = locale
+    // ？？ prettierrc 开头自动添加分号
+    false && ((i18n.global.locale as any).value = locale)
   }
   localeStore.setLocaleInfo({ locale })
   setHtmlPageLang(locale)
