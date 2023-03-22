b7e6e289fe789c9011a002f2a51b22dfce116521
nihaohaha <2797884436@qq.com>
2023-03-22 18:27:16 +0800

    feat: 添加功能路由

---
+++ src/router/routes/modules/features.ts
@@ -0,0 +1,51 @@
+import { AddRouteRecordRaw } from '../../types'
+import { t } from '@/hooks/web/useI18n'
+import ionLogoXbox from '@iconify-icons/ion/logo-xbox'
+const Layout = () => import('@/layout/index.vue')
+
+const features: AddRouteRecordRaw = {
+  path: '/feat',
+  name: 'component',
+  component: Layout,
+  redirect: '/feat/ws',
+  meta: {
+    orderNo: 30,
+    icon: {
+      name: 'ion:logo-xbox',
+      entity: ionLogoXbox
+    },
+    title: t('routes.demo.feat.feat')
+  },
+
+  children: [
+    {
+      path: 'ws',
+      name: 'WebSocket',
+      component: () => import('@/views/modules/feature/webSocket/index.vue'),
+      meta: {
+        title: t('routes.demo.feat.ws')
+      }
+    },
+    {
+      path: 'excel',
+      name: 'Excel',
+      redirect: '/feat/excel/importExcel',
+      component: Layout,
+      meta: {
+        title: t('routes.demo.excel.excel')
+      },
+      children: [
+        {
+          path: 'importExcel',
+          name: 'ImportExcel',
+          component: () => import('@/views/modules/feature/excel/ImportExcel.vue'),
+          meta: {
+            title: t('routes.demo.excel.importExcel')
+          }
+        }
+      ]
+    }
+  ]
+}
+
+export default features
